eb1d9e61830a50b6c86da9a0134393ec3af0bfe2
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2020-07-03 12:07:09 +0200

    🐛 Added missing providers dependencies for allowing tests to run

--- server/src/core/core.controller.spec.ts
+++ server/src/core/core.controller.spec.ts
@@ -1,11 +1,14 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CoreController } from './core.controller';
+import { CoreService } from './core.service';
+import { ParserService } from './parser/parser.service';
 
 describe('Core Controller', () => {
   let controller: CoreController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      providers: [CoreService, ParserService],
       controllers: [CoreController],
     }).compile();
 
