8b4a0ebb0230b2e5b470ecb4afd6ccb54392939f
Steven Noni <stivennoni77@gmail.com>
2020-07-06 14:28:03 +0200

    ♻️ use jest for tests & remove dead code

--- server/src/services/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as ping } from './ping'
-export { default as version } from './version'
--- server/src/utils/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as loadProto } from './loadProto'
-export { default as Service } from './Service'
